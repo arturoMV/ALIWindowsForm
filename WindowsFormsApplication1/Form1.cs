adb9705cbae1b4f02747390c32be7ebe9d7f56fe
madriart <madriart@MADRIART2>
2016-06-06 13:34:33 -0600

    implement requeriment #1 numero de clicks

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -12,9 +12,22 @@ namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
+        int numClicks = 0;
         public Form1()
         {
             InitializeComponent();
+            label1.Text += ""+numClicks; 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            numClicks++;
+            label1.Text = "Numero de clicks: " + numClicks;
         }
     }
 }
